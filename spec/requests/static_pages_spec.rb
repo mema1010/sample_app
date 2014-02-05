3dcff056c883fe97aff50a80d9143bc0a6fae5d9
Marco H <marcohuwe@gmx.de>
2014-02-05 14:27:51 +0100

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App' and titel 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help' and titel 'help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us' and titel 'about'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About")
+      expect(page).to have_content('About Us')
+    end
+  end
+end
