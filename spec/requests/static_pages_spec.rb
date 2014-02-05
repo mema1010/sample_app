2c86d1b61ee3c7a2ad04edb9494842f05613b77f
Marco H <marcohuwe@gmx.de>
2014-02-05 20:11:57 +0100

    add custom page titel with with application_helper.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,16 +4,20 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App' and titel 'Home'" do
+    it "should have the content 'Sample App' 
+        and titel 'Home'
+        and not the custom page title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Home")
+      expect(page).to have_title("Sample App")
       expect(page).to have_content('Sample App')
+      expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help' and titel 'help'" do
+    it "should have the content 'Help' 
+        and titel 'help'" do
       visit '/static_pages/help'
       expect(page).to have_title("Help")
       expect(page).to have_content('Help')
@@ -22,7 +26,8 @@ describe "Static pages" do
 
   describe "About page" do
 
-    it "should have the content 'About Us' and titel 'about'" do
+    it "should have the content 'About Us' 
+        and titel 'about'" do
       visit '/static_pages/about'
       expect(page).to have_title("About")
       expect(page).to have_content('About Us')
