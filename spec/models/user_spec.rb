ad4e8162a7724b396c5ffc06ef52b956f83c6a87
Marco H <marcohuwe@gmx.de>
2014-02-08 18:51:48 +0100

    fehler gefunden

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -57,7 +57,7 @@ describe User do
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
-      user_with_same_email = @user.email.upcase
+      user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
 
@@ -96,16 +96,17 @@ describe User do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
-  #describe "email should be save in downcase mode" do
-    #before do
-    #  user_with_upcase = @user
-    #  user_with_upcase.save
-    #end
-    #
-    #user_with_upcase.email == user.email
-      
-    #end
-  #end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+  
 end
 
 
